397dbe156266f49b6178f76c34e2134c24536ff9
Trung Nguyen <ntrung@pdx.edu>
2020-01-20 20:47:33 -0800

    Change file directory so it works on github

--- pokedex.js
+++ pokedex.js
@@ -28,16 +28,16 @@ var displayPokemon =(Pokemon)=>{
     <li class="card">
     <img class="card-image" src="${Pokemon.image}"/>
     <h2 class="card-title">${Pokemon.id}. ${Pokemon.name}</h2>
-    <input type="button" value="Click"  onclick="playaudio(${Pokemon})">
+    <input type="button" value="Click"  onclick="playaudio(${Pokemon.id})">
     <audio id="audio${Pokemon.id}" src="../FrontEnd_Pokedex/sound/${Pokemon.id}.wav" ></audio>
     </li>
     `;
     pokedex.insertAdjacentHTML('beforeend',pokemonHTMLString);
 };
 
-var playaudio= (Pokemon)=>
+var playaudio= (id)=>
 {
-    var audio = document.getElementById("audio${Pokemon.id}");
+    var audio = document.getElementById("audio"+id);
     audio.play();
 }
 getAllPokemons();
